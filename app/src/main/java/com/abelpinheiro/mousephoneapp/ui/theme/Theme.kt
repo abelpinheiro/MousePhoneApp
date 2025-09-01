49e92e83ef626841bc26936d9375665241ee0d6d
abel pinheiro de figueiredo <abelpinheiro95@gmail.com>
2025-09-01 00:11:55 -0300

    light theme adjusted

--- app/src/main/java/com/abelpinheiro/mousephoneapp/ui/theme/Theme.kt
+++ app/src/main/java/com/abelpinheiro/mousephoneapp/ui/theme/Theme.kt
@@ -23,26 +23,19 @@ private val DarkColorScheme = darkColorScheme(
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
+    primary = Color(0xFF6A5AE0),
+    background = Color(0xFFF0F2F5),
+    surface = Color.White,
     onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+    onBackground = Color.Black,
+    onSurface = Color.Black,
 )
 
 @Composable
 fun MousePhoneAppTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
+    dynamicColor: Boolean = false,
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
